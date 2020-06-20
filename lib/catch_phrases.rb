b8c772a2e5518c80e3a6f1cf2c2957e9783c851b
Jacob Squier <jacob.squier@gmail.com>
2020-06-20 07:14:59 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -3,16 +3,19 @@
 def mario
   phrase = "It's-a me, Mario!"
   puts phrase
+  return phrase
 end
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+  return status
 end
 
 def link 
   catchPhrase = "It's Dangerous To Go Alone! Take This."
   puts catchPhrase
+  return catchPhrase
 end
 
 def all_phrases 
